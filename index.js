7e3165a6966e7fc806dcb4ca22ceba0aa9cd6493
Adrien Pessu <adrien@pessu.net>
2019-09-01 14:28:22 +0200

    add users

--- index.js
+++ index.js
@@ -33,29 +33,50 @@ async function writeUsers() {
   return kitty.save();
 }
 
+function arr_diff (a1, a2) {
+
+  var a = [], diff = [];
+
+  for (var i = 0; i < a1.length; i++) {
+      a[a1[i]] = true;
+  }
+
+  for (var i = 0; i < a2.length; i++) {
+      if (a[a2[i]]) {
+          delete a[a2[i]];
+      } else {
+          a[a2[i]] = true;
+      }
+  }
+
+  for (var k in a) {
+      diff.push(k);
+  }
+
+  return diff;
+}
+
 async function checkList() {
   const list = await List.find({}).sort({date:1}).limit(2);
   if(list.length > 1){
-    //if(list[1].users.length - list[0].users.length > 0){
+    if(list[1].users.length - list[0].users.length > 0){
+      const diff = arr_diff(list[0].users, list[1].users).filter(user => list[1].users.map(user => user.id).indexOf(user.id) > 0);
       await fetch("https://api.pushbullet.com/v2/pushes", {
-        body: `{\"body\":\"${list[1].users.length - list[0].users.length} followers en moins\",\"title\":\"Unfollowing\",\"type\":\"note\"}`,
+        body: `{\"body\":\"${list[1].users.length - list[0].users.length} followers en moins, ${diff.map(user => user.name + ' ' + user.screen_name).join(', ')}\",\"title\":\"Unfollowing\",\"type\":\"note\"}`,
         headers: {
           "Access-Token": process.env.PUSHBULLET,
           "Content-Type": "application/json"
         },
         method: "POST"
       })
-    //}     
+    }     
   }
 }
 
 async function main() {
   try{
-    console.log('step 1')
     await writeUsers();
-    console.log('step 2')
     await checkList();
-    console.log('the end')
     process.exit()
   } catch(err ){
     console.error(err)
